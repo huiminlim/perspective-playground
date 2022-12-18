cc96ea3f5de402276a7afee52a66f0dca7e42a96
huiminlim <e0036379@u.nus.edu>
2022-12-19 00:12:12 +0800

    Add default values for fields

---
+++ app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data() {
+        return {
+            perspective: 500,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0
+        }
+    },
+}).mount("#app")
